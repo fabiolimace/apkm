212279a74a9f1e9601a519d10ac39972ac2e23dc
Fabio Lima <fabiolimace@gmail.com>
2024-03-17 07:52:46 -0300

    Add apkm-save.sh

---
+++ bin/apkm-save.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Saves metadata and links in `meta` folder and `meta.db`.
+#
+# Usage:
+#
+#     apwm-save.sh FILE
+#
+
+source "`dirname "$0"`/apkm-common.sh" || exit 1;
+validate_program_and_working_paths || exit 1;
+
+FIND_REGEX=".*.\(md\)$";
+IGNORE_REGEX="\\.\(apkm\|git\)";
+
+while read -s line; do
+
+    FILE=`echo $line | sed 's,^\./,,'`; # remove leading "./"
+    
+    "$PROGRAM_DIR/apkm-save-meta.sh" "$FILE";
+    "$PROGRAM_DIR/apkm-save-links.sh" "$FILE";
+
+done < <(cd "$WORKING_DIR"; find . -type f -regex "$FIND_REGEX" | grep -v "$IGNORE_REGEX");
+
